e810b0e4035e24b11b1b385acd29e00162f07ea4
MUNYESHURImanzi <munyeshurimanzi@gmail.com>
2023-07-12 03:33:06 -0700

    project update

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 form=document.querySelector("#submit")
 
 form.addEventListener("click",(e)=>{
@@ -6,22 +8,22 @@ form.addEventListener("click",(e)=>{
     var weight= parseFloat(document.querySelector("#weight").value)
     var bodymass= weight/(height*height)
     console.log(bodymass);
-     var b=document.querySelector(".sec2 button");
+     var b=document.querySelector(".sec2 #output");
     b.innerHTML=bodymass
     if (bodymass<=18.5) {
-       var under= document.querySelector(".sec2 button")
+       var under= document.querySelector(".sec2 #output")
        under.innerHTML=" Underweight: Your BMI is less than 18.5 "
     }
     else if (bodymass>=18.5 && bodymass<=24.9) {
-        var under= document.querySelector(".sec2 button")
+        var under= document.querySelector(".sec2 #output")
         under.innerHTML="  Healthy weight: Your BMI is 18.5 to 24.9"
     }
     else if (bodymass>=25 && bodymass<=29.9 ) {
-        var under= document.querySelector(".sec2 button")
+        var under= document.querySelector(".sec2 #output")
         under.innerHTML="   Overweight: Your BMI is 25 to 29.9"
     }
     else if (bodymass>=30  ) {
-        var under= document.querySelector(".sec2 button")
+        var under= document.querySelector(".sec2 #output")
         under.innerHTML="    Obese:  Your BMI is 30 or higher "
     }
     else if (weight !== '' || height !== '') {
